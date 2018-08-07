ee0b080b07ee1765dd65a096b403c8315269fb28
Kiselev Anton <j8kiselev@gmail.com>
2018-08-07 20:04:09 +0500

    injection literal values

--- src/main/java/kiselev/springdemo/CricketCoach.java
+++ src/main/java/kiselev/springdemo/CricketCoach.java
@@ -1,6 +1,12 @@
 package kiselev.springdemo;
 
-public class CricketCoach implements Coach{
+public class CricketCoach implements Coach {
+
+    //add new fields for emailAddress and team
+
+    private String emailAddress;
+
+    private String team;
 
     private FortuneService fortuneService;
 
@@ -8,6 +14,24 @@ public class CricketCoach implements Coach{
 
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 
     //our setter method
