8f063591213346ee31ff5bf986b3666271da7635
Kiselev Anton <j8kiselev@gmail.com>
2018-08-17 12:10:08 +0500

    @Scope annotation

---
+++ src/main/java/kiselev/annotations/AnnotationBeanScopeDemoApp.java
@@ -0,0 +1,31 @@
+package kiselev.annotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationBeanScopeDemoApp {
+
+    public static void main(String[] args) {
+
+        // load spring config file
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("spring/annotations-applicationContext.xml");
+
+        // retrieve a bean spring container
+        Coach theCoach = context.getBean("tennisCoach", Coach.class);
+
+        Coach alphaCoach = context.getBean("tennisCoach", Coach.class);
+
+        // check if they are the same
+        boolean result = (alphaCoach == theCoach);
+
+        // print out the results
+        System.out.println("\nPointing to the same object: " + result);
+
+        System.out.println("\nMemory location for theCoach: " + theCoach);
+
+        System.out.println("\nMemory location for theCoach: " + alphaCoach);
+
+        // close the context
+        context.close();
+    }
+}
